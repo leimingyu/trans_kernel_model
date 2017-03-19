6d99c520d0524e4a4cd6d69d6f9367ca5302d8f3
Leiming Yu <leimingyu830@gmail.com>
2017-03-19 16:11:35 -0400

    update trans time for concurrent h2d

--- mem_mem/df_util.py
+++ mem_mem/df_util.py
@@ -6,8 +6,8 @@ import sys
 
 #------------------------------------------------------------------------------
 # Update Cell Function.
-# Example:
-# (old)  
+# 
+# Example (old)  
 # df_all_api.iloc[0, df_all_api.columns.get_loc('time_left')] = current_predict_time_left
 # (now)
 # df_all_api = updateCell(df_all_api, 0, 'time_left', current_predict_time_left)
@@ -16,3 +16,28 @@ def UpdateCell(df_all_api, row_id, col_name, val):
     df = df_all_api.copy(deep=True)
     df.iloc[row_id, df.columns.get_loc(col_name)] = val
     return df
+
+
+#------------------------------------------------------------------------------
+# Update transfer timing: bytes_left, time_left, predicted end time
+#------------------------------------------------------------------------------
+def UpdateTranTime(df_all, row_list, ways = 1.0):
+    """
+    From the input dataframe, adjust the row info, depending on the concurrency.
+    """
+    df_all_api = df_all.copy(deep=True)
+
+    cc = ways # concurrency
+
+    for i in row_list:
+        # the bandwidth is shared among all the concurrent transfer
+        bw = df_all_api.iloc[i]['bw'] / cc
+        # predict the transfer time based on the bandwidth
+        cur_pred_time_left = df_all_api.iloc[i]['bytes_left'] / bw
+        # update the cell: time_left 
+        df_all_api = UpdateCell(df_all_api, i, 'time_left', cur_pred_time_left)
+        # update the future ending time
+        df_all_api = UpdateCell(df_all_api, i, 'pred_end', 
+                cur_pred_time_left + df_all_api.iloc[i]['current_pos'] )
+    
+    return df_all_api
