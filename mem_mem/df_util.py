db342f2a70e22dca6d308f39afe65aed916765c5
Leiming Yu <leimingyu830@gmail.com>
2017-03-21 09:33:12 -0400

    up

--- mem_mem/df_util.py
+++ mem_mem/df_util.py
@@ -7,15 +7,11 @@ from math import *
 
 #------------------------------------------------------------------------------
 # Update Cell Function.
-# 
-# Example (old)  
-# df_all_api.iloc[0, df_all_api.columns.get_loc('time_left')] = current_predict_time_left
-# (now)
-# df_all_api = updateCell(df_all_api, 0, 'time_left', current_predict_time_left)
 #------------------------------------------------------------------------------
 def UpdateCell(df_all_api, row_id, col_name, val):
     df = df_all_api.copy(deep=True)
-    df.iloc[row_id, df.columns.get_loc(col_name)] = val
+    #df.iloc[row_id, df.columns.get_loc(col_name)] = val
+    df.set_value(row_id, col_name, val)
     return df
 
 
