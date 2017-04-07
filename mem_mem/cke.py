c00e713b37dbae8c4cb9b3fdb794354b04eaf91d
Leiming Yu <leimingyu830@gmail.com>
2017-04-07 00:29:50 -0400

    up

--- mem_mem/cke.py
+++ mem_mem/cke.py
@@ -916,11 +916,6 @@ def check_activestream_and_update(df_all, activestream_dd, simPos):
     # end the target row, update the bytes for other call
     df = end_target_row(df, row2end, simPos, nextCall_start)
 
-    ## how many h2d ovlp
-    #h2d_list, d2h_list, kern_list = FindOvlp(df, wake_list)
-    #print h2d_list
-    #print d2h_list
-    #print kern_list
 
 
 
