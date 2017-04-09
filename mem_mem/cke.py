188946631fd3a272ec1f4f953c096e019d3c51b0
Leiming Yu <leimingyu830@gmail.com>
2017-04-09 11:19:10 -0400

    up

--- mem_mem/cke.py
+++ mem_mem/cke.py
@@ -942,7 +942,6 @@ def update_by_range(df_all, begT, endT, Gpu, SM_resList, SM_traceList, stream_ke
         if kern_list_size > 1:
 
             #sys.stderr.write('kernel model no accomplished yet!')
-
             for i in range(0, kern_list_size):
                 cur_row = sorted_kerns[i]
                 pre_row = sorted_kerns[i-1]
